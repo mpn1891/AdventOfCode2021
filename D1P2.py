9dd1c9a0156ba66cec13deb514841123117fbc25
Matt Martin <mattmartin1891@gmail.com>
2021-12-01 07:38:12 -0500

    Day 1
    Commit to include Day 1 files

--- D1P2.py
+++ D1P2.py
@@ -1,17 +1,22 @@
 
+
 array = []
 answer = 0
+windowA = 0
+windowB = 0
 with open(r"C:\Users\mpn42\source\repos\AdventOfCode2021\AdventOfCode2021\Input Files\D1.txt","r") as f:
     for val in f.read().split():
         array.append(int(val))  
         
-print(array)
 
-for i in range(len(array)-1):
-    if array[i+1] > array[i]:
+for i in range(len(array)-3):
+    windowA = array[i] + array[i+1] + array[i+2]
+    windowB = array[i+1] + array[i+2] +array[i+3]
+
+    if windowB > windowA:
         answer =+ answer + 1
-        continue
-    else:
-        continue
+
+    windowA = 0
+    windowB = 0
 
 print (answer)
